e01f620eb6aab6a47c65e433d33d01620eafcf12
Daniel Duque <daniel.duque@gmail.com>
2021-06-29 12:13:16 +0200

    slight mods

--- tools/draw_traj.py
+++ tools/draw_traj.py
@@ -19,8 +19,8 @@ plt.scatter( x , y , 10, c=p )
 plt.xlabel(r'$x$')
 plt.ylabel(r'$y$')
 
-plt.xlim([-LL/2.0 , LL/2.0 ])
-plt.ylim([-LL/2.0 , LL/2.0 ])
+plt.xlim([-LL/8.0 , LL/8.0 ])
+plt.ylim([-LL/8.0 , LL/8.0 ])
 plt.colorbar()
 
 plt.savefig( 'traj.png' )
